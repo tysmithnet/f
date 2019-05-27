5c744c7aab58e816a4a8e74baba246fb3aa41ef8
Tyler Smith <dts5004@gmail.com>
2019-05-27 03:59:50 -0700

    Created shell using tui-go

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-	"bufio"
-	"io"
-	"os"
+	"log"
 
-	"github.com/rivo/tview"
+	"github.com/marcusolsson/tui-go"
 )
 
-var buffer []string
-
-/* Function to run the groutine to run for stdin read */
-func read(r io.Reader) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer close(lines)
-		scan := bufio.NewScanner(r)
-		for scan.Scan() {
-			s := scan.Text()
-			lines <- s
-		}
-	}()
-	return lines
-}
-
 func main() {
-	ch := read(os.Stdin) //Reading from Stdin
-	go func() {
-		for m := range ch {
-			buffer = append(buffer, m)
-		}
-	}()
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+	output := tui.NewVBox()
+	outputScroll := tui.NewScrollArea(output)
+	outputScroll.SetAutoscrollToBottom(true)
+	outputBox := tui.NewVBox(outputScroll)
+	outputBox.SetBorder(true)
+	input := tui.NewEntry()
+	input.SetFocused(true)
+	input.SetSizePolicy(tui.Expanding, tui.Maximum)
+	input.OnChanged(func(e *tui.Entry) {
+		output.Append(tui.NewLabel(e.Text()))
+	})
+	inputBox := tui.NewHBox(input)
+	inputBox.SetBorder(true)
+	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+	root := tui.NewVBox(outputBox, inputBox)
+	ui, err := tui.New(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ui.SetKeybinding("Esc", func() {
+		ui.Quit()
+	})
+
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
