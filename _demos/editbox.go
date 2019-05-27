0db4615b1bce1d2ddaa115b65dc62fe5a481ebaa
Tyler Smith <dts5004@gmail.com>
2019-05-27 16:42:56 -0700

    Copied code from termbox-go

--- _demos/editbox.go
+++ _demos/editbox.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"unicode/utf8"
-
 	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
+	"unicode/utf8"
 )
 
 func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
