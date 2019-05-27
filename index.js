e55939ac341fe4ec3a087620ab26a0c4b9a4a176
Tyler Smith <dts5004@gmail.com>
2019-05-26 23:28:54 -0700

    Vue + blessed example

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-const process = require("process");
-const readline = require("readline");
-const blessed = require('blessed');
-const contrib = require('blessed-contrib');
-const screen = blessed.screen({
-    smartCSR: true,
-});
+// Make sure to import from 'blessed-vue' instead of 'vue'!
+import Vue from 'blessed-vue';
+import App from './App.vue';
 
-const grid = new contrib.grid({ rows: 20, cols: 1, screen: screen });
-const input = grid.set(0, 0, 1, 1, blessed.textbox, { label: "Input!", height: "100%", width: "100%", style: {fg: "green"} })
-const box = grid.set(1, 0, 19, 1, blessed.box, { label: "Box!", height: "100%", width: "100%" })
-const buffer = [];
+// Create a new fake root element. (Since blessed has no real DOM)
+const el = Vue.dom.createElement();
+Vue.dom.append(el);
 
-input.focus();
-screen.render();
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
-});
-
-rl.on("line", (line) => {
-    buffer.push(line);
-    box.setContent(line);
-    screen.render();
-});
+new Vue({
+  render: h => h(App)
+})
+// Mount to the fake element.
+.$mount(el);
