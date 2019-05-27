80b336a385bb56893f6b452838f76ad1425c0cfc
Tyler Smith <dts5004@gmail.com>
2019-05-26 17:14:51 -0700

    Added rough outline for processing

--- src/f.py
+++ src/f.py
@@ -3,3 +3,18 @@ def main():
 
 if __name__ == "__main__":
     print(main())
+
+"""
+f
+ - get data
+ - update buffer
+ - apply current filter
+ - update output buffer if necessary
+
+ on filter change:
+  - kill subprocess if necessary
+  - clear output buffer
+  - start new filter process for current data set
+  - update display
+  - if more data since update goto 0, but only new data and update the display
+"""
